04e0c649f5029a7fd4d3d9d08ecd908041b475b1
YEONWOO-IAN-PARK <choestro@gmail.com>
2023-02-14 16:15:50 +0900

    Interceptor & Login

--- src/main/java/hello/login/web/WebConfig.java
+++ src/main/java/hello/login/web/WebConfig.java
@@ -2,14 +2,25 @@ package hello.login.web;
 
 import hello.login.web.filter.LogFilter;
 import hello.login.web.filter.LoginCheckFilter;
+import hello.login.web.interceptor.LogInterceptor;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.Filter;
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**") // 모든 하위경로 전부 다
+                .excludePathPatterns("/css/**", "/*.ico", "error");
+    }
 
     // Spring Boot은 WAS를 들고있기 떄문에 아래 방식으로 빈설정을 해주면 필터가 등록이 된다.
     @Bean
